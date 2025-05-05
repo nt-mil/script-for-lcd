5466a96c14b3f14863240e2c4dcd30a4475606e7
ntmil247 <ngoctrieu240799@gmail.com>
2025-05-06 00:04:23 +0700

    upload base code

---
+++ Core/Inc/macro.h
@@ -0,0 +1,22 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+typedef unsigned char       boolean;
+typedef unsigned char       u8;
+typedef signed char         s8;
+typedef unsigned short      u16;
+typedef signed short        s16;
+typedef unsigned int        u32;
+typedef signed int          s32;
+typedef unsigned long long  u64;
+
+#define OFF     (0)
+#define ON      (1)
+
+#define FALSE   (0)
+#define TRUE    (1)
+
+#define READ    (0)
+#define WRITE   (1)
+
+#endif /* MACRO_H */

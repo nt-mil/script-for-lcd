3a79ecf6eedfefb93eb46fb581b0ca704bd0f21b
nt-mil <ngoctrieu240799@gmail.com>
2025-07-19 14:15:58 +0700

    implement display_info structure

--- Drivers/Display/dev_display.h
+++ Drivers/Display/dev_display.h
@@ -2,10 +2,15 @@
 #define DEV_LCD_H
 
 typedef struct {
+    uint8_t* data;
+    uint16_t size;
+} display_info_t;
+
+typedef struct {
     void (*init)(void);
     // void (*update)(void);
     // void (*update_window)(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
-    uint8_t* (*get_framebuffer)(void);
+    display_info_t* (*get_framebuffer)(void);
 } display_driver_t;
 
 #endif /* DEV_LCD_H */
